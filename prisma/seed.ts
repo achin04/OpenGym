2cbd4e651a08c5ebbaad48b653a7557e57f11cfa
Aaron <aaronchin111@gmail.com>
2026-05-13 18:25:34 -0400

    milestone 2 complete

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,14 +1,22 @@
-import { PrismaClient, SourceType, SkillLevel, AgeGroup } from "../src/generated/prisma";
+import "dotenv/config";
+import { PrismaClient, RunSourceType, SkillLevel, AgeGroup } from "../src/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const prisma = new PrismaClient({
+  adapter,
+});
+
 
 async function main() {
     const communityCenter = await prisma.venue.create({
         data: {
             name: "Downtown Community Center",
-            address: "100 Queen Street West,",
+            addressLine1: "100 Queen Street West,",
             city: "Toronto",
-            province: "ON",
             postalCode: "M5H 2N2",
         },
     });
@@ -16,11 +24,53 @@ async function main() {
     const universityGym = await prisma.venue.create({
         data: {
             name: "University Athletic Centre",
-            address: "55 College Street",
+            addressLine1: "55 College Street",
             city: "Toronto",
-            province: "ON",
             postalCode: "M5G 2B3",
         },
     });
-        
+
+    await prisma.run.create({
+        data: {
+            title: "Friday Night Open Run",
+            description: "Join us for an evening of fun and fitness!",
+            sourceType: RunSourceType.CITY,
+            startTime: new Date("2024-07-05T19:00:00Z"),
+            endTime: new Date("2024-07-05T21:00:00Z"),
+            price: 10,
+            skillLevel: SkillLevel.INTERMEDIATE,
+            ageGroup: AgeGroup.ADULT,
+            maxPlayers: 20,
+            verified: true,
+            sourceUrl: "https://www.toronto.ca/events/friday-night-open-run/",
+            venueId: communityCenter.id,
+        },
+    });
+
+    await prisma.run.create({
+        data: {
+        title: "Sunday University Pickup",
+        description: "Indoor pickup basketball at the university gym.",
+        sourceType: RunSourceType.UNIVERSITY,
+        startTime: new Date("2026-06-07T15:00:00.000Z"),
+        endTime: new Date("2026-06-07T17:00:00.000Z"),
+        price: 5,
+        skillLevel: SkillLevel.BEGINNER,
+        ageGroup: AgeGroup.ADULT,
+        maxPlayers: 16,
+        verified: true,
+        sourceUrl: "https://example.com/sunday-pickup",
+        venueId: universityGym.id,
+        },
+    });       
 }
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (error) => {
+        console.error(error);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
